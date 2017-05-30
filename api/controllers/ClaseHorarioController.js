72e5b570d13ef4397e03d78843cd4253ec2f6737
Brandon Neri <arcaniteamp@gmail.com>
2017-05-30 12:54:12 -0500

    Muchas weas x2

--- api/controllers/ClaseHorarioController.js
+++ api/controllers/ClaseHorarioController.js
@@ -5,6 +5,19 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+const moment = require('moment');
+
 module.exports = {
+
+  getMyHorario: (req, res) => {
+    let id = req.param('id');
+    let dia = moment().weekday();
+    ClaseHorario.find({dia: dia, grupo: id}).populateAll().then(function(data) {
+      return res.json(data);
+    }).catch(function(err) {
+      console.log(err);
+      return res.json(500, {msg: err});
+    });
+  }
 
 };
